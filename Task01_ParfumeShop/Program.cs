3dbf39077d219da4422dd4010a952500c758834e
Sofia Masliak <sofiia108@icloud.com>
2020-02-21 17:40:44 +0200

    Finish task 1

--- Task01_ParfumeShop/Program.cs
+++ Task01_ParfumeShop/Program.cs
@@ -6,7 +6,25 @@ namespace Task01_ParfumeShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[,] locker = new string[2, 3];
+            locker[0, 0] = "Fendy";
+            locker[0, 1] = "Versace";
+            locker[0, 2] = "Gucci";
+            locker[1, 0] = "Invictus";
+            locker[1, 1] = "№13";
+            Console.WriteLine("Enter the last parfume");
+            locker[1, 2] = Console.ReadLine();
+            for (var j = 0; j <= locker.GetUpperBound(0); j++)
+            {
+                Console.WriteLine();
+                for (var i = 0; i <= locker.GetUpperBound(1); i++)
+                    Console.Write($"[{i}],[{j}] {locker[j, i]} \t\t");
+            }
+
+
+
+
+
         }
     }
 }
