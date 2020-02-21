6203fecef20809e85a6936596870425502ce71e8
Sofia Masliak <sofiia108@icloud.com>
2020-02-21 16:39:14 +0200

    Initial commit

---
+++ Task01_ParfumeShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task01_ParfumeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
